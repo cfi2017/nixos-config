860beccd26724319d60b99849919d1d347f1be86
Carlo Field <carlo@swiss.dev>
2025-11-27 11:14:16 +0100

    feat: add goland and rustrover

--- modules/nixos/desktop/applications/default.nix
+++ modules/nixos/desktop/applications/default.nix
@@ -1,1 +1,18 @@
-{ ... }: { imports = [ ]; }
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+  ];
+
+  config = {
+    home-manager.users.${config.cfi2017.user.name} = {
+      home.packages = [
+        pkgs.jetbrains.rust-rover
+        pkgs.jetbrains.goland
+      ];
+    };
+  };
+}
