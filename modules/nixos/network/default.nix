2e87e27492fd3d3ffda5ae030fc085d0adcdc403
Carlo Field <carlo@swiss.dev>
2026-01-30 17:20:46 +0100

    fix: disable dnssec
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -48,13 +48,15 @@
       ];
 
       networking.nameservers = [
-        "1.1.1.1#one.one.one.one"
-        "1.0.0.1#one.one.one.one"
+        # "1.1.1.1#one.one.one.one"
+        # "1.0.0.1#one.one.one.one"
+        "1.1.1.1"
+        "8.8.8.8"
       ];
 
       services.resolved = {
         enable = true;
-        dnssec = "true";
+        dnssec = "false";
         domains = [ "~." ];
         fallbackDns = [
           "1.1.1.1#one.one.one.one"
