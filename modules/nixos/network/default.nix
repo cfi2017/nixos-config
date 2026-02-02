8c0d58480512962bd963f2f6a82123339881ea5d
Carlo Field <carlo@swiss.dev>
2026-02-02 12:29:45 +0100

    fix: update dns config
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -59,10 +59,10 @@
         dnssec = "false";
         domains = [ "~." ];
         fallbackDns = [
-          "1.1.1.1#one.one.one.one"
-          "1.0.0.1#one.one.one.one"
+          "1.1.1.1"
+          "1.0.0.1"
         ];
-        dnsovertls = "true";
+        dnsovertls = "false";
       };
     }
   ];
