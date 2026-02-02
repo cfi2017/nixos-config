1cd96f89aa54c5402bd379700562c652ed55643e
Carlo Field <carlo@swiss.dev>
2026-02-02 15:59:22 +0100

    feat: enable just the mail client
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/shared/email/default.nix
+++ modules/shared/email/default.nix
@@ -33,41 +33,8 @@ in
                 notmuch = {
                   enable = true;
                   hooks = {
-                    preNew = "mbsync --all";
+                    # preNew = "mbsync --all";
                   };
-                };
-              };
-
-              accounts.email.accounts = {
-                "sample" = {
-                  address = "";
-                  gpg = {
-                    key = "";
-                    signByDefault = "";
-                  };
-
-                  flavor = "gmail.com";
-
-                  lieer = {
-                    enable = true;
-                    sync = {
-                      enable = true;
-                    };
-                  };
-                  mbsync = {
-                    enable = false;
-                    create = "maildir";
-                  };
-                  msmtp.enable = true;
-                  notmuch.enable = true;
-                  primary = true;
-                  realName = "";
-                  signature = {
-                    text = '''';
-                    showSignature = "append";
-                  };
-                  passwordCommand = "mail-password";
-                  userName = config.sops.secrets."email/${config.cfi2017.email.accounts.one.name}".path;
                 };
               };
             };
