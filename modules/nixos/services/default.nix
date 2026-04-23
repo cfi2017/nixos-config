bf2253ea7460658bdac5d324d0f058561eafc595
Carlo Field <carlo@swiss.dev>
2026-04-23 15:11:55 +0200

    fix: disable pcscd
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [ ./sshd.nix ];
   services.usbmuxd.enable = true;
-  services.pcscd.enable = true;
+  services.pcscd.enable = false;
   services.fprintd.enable = true;
   services.printing.enable = true;
   services.hardware.bolt.enable = true;
