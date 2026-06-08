534c8b0521bb900c220e2d4b16790f9b1f814d0a
Carlo Field <carlo@swiss.dev>
2026-06-08 09:25:48 +0200

    feat: block pcscd from seeing yubikey
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -2,7 +2,14 @@
 {
   imports = [ ./sshd.nix ];
   services.usbmuxd.enable = true;
-  services.pcscd.enable = false;
+  services.pcscd.enable = true;
+  # prevent yubikey locking when pcscd is enabled
+  services.udev.extraRules = ''
+    # YubiKey 5 NFC / 5C NFC
+    ATTR{idVendor}=="1050", ATTR{idProduct}=="0407", ENV{ID_SMARTCARD_READER}="", ENV{ID_SMARTCARD_READER_DRIVER}=""
+    # YubiKey 5C
+    ATTR{idVendor}=="1050", ATTR{idProduct}=="0406", ENV{ID_SMARTCARD_READER}="", ENV{ID_SMARTCARD_READER_DRIVER}=""
+  '';
   services.fprintd.enable = true;
   services.fprintd.tod = {
     enable = true;
