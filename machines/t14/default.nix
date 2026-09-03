0a4305846f8751abd359a32ec0c660b9df376526
Carlo Field <carlo@swiss.dev>
2026-09-03 15:40:27 +0200

    feat: add support for t14
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- machines/t14/default.nix
+++ machines/t14/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./disko.nix
@@ -14,7 +14,43 @@
       "1.1.1.1"
       "8.8.8.8"
     ];
+    useDHCP = lib.mkDefault true;
+    networkmanager.enable = true;
   };
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd.systemd.enable = true;
+  };
+
+  console = {
+    earlySetup = true;
+    useXkbConfig = true;
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    enableAllFirmware = true;
+    graphics.enable = true;
+    # The shared Hyprland module currently defaults to NVIDIA. Keep enrollment
+    # vendor-neutral until the generated hardware configuration is reviewed.
+    nvidia.open = false;
+  };
+
+  services = {
+    blueman.enable = true;
+    fstrim.enable = true;
+    tlp.enable = true;
+    xserver = {
+      videoDrivers = lib.mkForce [ "modesetting" ];
+      xkb.layout = "ch";
+    };
+  };
+
+  system.stateVersion = "26.05";
 
   cfi2017 = {
     stateVersion = "26.05";
