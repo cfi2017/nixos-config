f30a41c879bc6da29a5b5c61cf8f0d5ebbd15f34
Carlo Field <carlo@swiss.dev>
2026-09-07 13:00:43 +0200

    fix: displays
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/nixos/desktop/desktop-environment/niri.nix
+++ modules/nixos/desktop/desktop-environment/niri.nix
@@ -163,23 +163,35 @@ in
       # --- office: twin landscape S24H85x followed by a portrait Legion
       # The Legion is 3200 logical pixels tall after rotation at 0.8 scale.
       # Centre the 1440-high Samsungs against it: (3200 - 1440) / 2 = 880.
-      "Samsung Electric Company S24H85x H4ZKC00255" = {
+      # "Samsung Electric Company S24H85x H4ZKC00255" = {
+      #   position = {
+      #     x = 1920;
+      #     y = 880;
+      #   };
+      # };
+      # "Samsung Electric Company S24H85x H4ZMA00945" = {
+      #   position = {
+      #     x = 4480;
+      #     y = 880;
+      #   };
+      # };
+      "Samsung Electric Company LC34G55T HNTYC01410" = {
         position = {
-          x = 1920;
+          x = 0;
           y = 880;
         };
-      };
-      "Samsung Electric Company S24H85x H4ZMA00945" = {
-        position = {
-          x = 4480;
-          y = 880;
+
+        layout = {
+          default-column-width = {
+            proportion = 0.33333;
+          };
         };
       };
       "Lenovo Group Limited Legion 27Q-10 UPACG819" = {
         scale = 0.8;
         transform = "270";
         position = {
-          x = 7040;
+          x = 3440;
           y = 0;
         };
       };
